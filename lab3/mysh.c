6c51bbe16416156ae1fa5a3e27e324ee1ab58d66
Laura-Neff <ljneff@gmail.com>
2021-04-10 16:49:52 -0400

    Added algorithm.

--- lab3/mysh.c
+++ lab3/mysh.c
@@ -2,3 +2,17 @@
 // THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
 // A TUTOR OR CODE WRITTEN BY OTHER STUDENTS - LAURA NEFF
 
+while(1) {
+    // commands = getCmd();
+    // foreach command in commands: create succeeding pipefor I/O*
+    // create child processopen files for I/O*
+    // redirect stdin from file or preceding pipe*
+    // redirect stdoutto file or succeeding*
+    // execute command in child
+    // track childpid in parent
+    // parent waits for foreground children*
+
+
+
+
+}
