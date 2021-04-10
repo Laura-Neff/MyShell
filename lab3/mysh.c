9cb4b9a6ba184d41d5f7dbc4453c8625cf1696ae
Laura Neff <40323885+Laura-Neff@users.noreply.github.com>
2021-04-10 16:51:32 -0400

    Starter mysh.c file

--- lab3/mysh.c
+++ lab3/mysh.c
@@ -1,18 +1,33 @@
-
 // THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
 // A TUTOR OR CODE WRITTEN BY OTHER STUDENTS - LAURA NEFF
 
-while(1) {
-    // commands = getCmd();
-    // foreach command in commands: create succeeding pipefor I/O*
-    // create child processopen files for I/O*
-    // redirect stdin from file or preceding pipe*
-    // redirect stdoutto file or succeeding*
-    // execute command in child
-    // track childpid in parent
-    // parent waits for foreground children*
+#include <stdio.h> //file i/o commands
+#include <sys/stat.h> //checking file types of file descriptors
+#include <sys/types.h> //fstat, other file i/o, wait
+#include <sys/time.h> //for wait
+#include <sys/resource.h> //for wait
+#include <sys/wait.h> //for eait
+#include <getopt.h> //for getopt
+#include <unistd.h> //pipe command, parsing command line stuff, pause, forking
+#include <stdlib.h> //malloc and stuff
+#include <errno.h> //for the errno checking
+#include <fcntl.h> //for O_CONSTANTS
+#include <string.h> //fprintf, etc.
+
+#include "tokens.h" //custom tokenizer made for the class
 
 
+int main( int argc, char *argv[] )
+{
+    while(1) {
+        // commands = getCmd();
+        // foreach command in commands: create succeeding pipefor I/O*
+        // create child processopen files for I/O*
+        // redirect stdin from file or preceding pipe*
+        // redirect stdoutto file or succeeding*
+        // execute command in child
+        // track childpid in parent
+        // parent waits for foreground children*
+    }
 
-
-}
+};
