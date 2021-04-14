1330c1abfb71e9a36c3601b7cd3e356102fd1b3e
Laura-Neff <ljneff@gmail.com>
2021-04-13 23:21:51 -0400

    Added another output error message.

--- lab3/mysh.c
+++ lab3/mysh.c
@@ -202,7 +202,7 @@ int main( int argc, char *argv[] )
                         command->outputFile = breakup[i];
                         //fprintf(stdout,"inputting from: %s\n",new_command->input);
                     } else {
-                        fprintf(stderr,"Error: No output specified.\n");
+                        fprintf(stderr,"Error: Missing filename for output redirection.\n");
                         error=1;
                         break;
                     }
